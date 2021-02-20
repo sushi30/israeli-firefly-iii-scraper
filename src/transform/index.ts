c6b2a7add5812a1e015b7622211cfd5df1470939
imri <imri.paran@gmail.com>
2021-02-20 13:29:07 +0200

    fixed consolle log done

--- src/transform/index.ts
+++ src/transform/index.ts
@@ -3,8 +3,7 @@ import * as path from "path";
 import transform from "./transform";
 
 export default async function main({ directory, host }) {
-  console.log(`done`);
   const txns = fs.readdirSync(directory).map((f) => JSON.parse(fs.readFileSync(path.join(directory, f)).toString())).map(transform)
-  
   console.log(txns)
+  console.log(`done`);
 }
