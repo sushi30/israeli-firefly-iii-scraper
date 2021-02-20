671eeff5a23047a983476ade960aceb7b6d372d2
imri <imri.paran@gmail.com>
2021-02-20 14:02:59 +0200

    added ouptut directory

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ program
   .version("0.0.1")
   .command("scrape")
   .requiredOption("-s, --start <date>", "start date")
-  .requiredOption("-e, --end <end>", "end date")
   .requiredOption("-t, --type <type>", "type of scraper")
   .option("-h, --headless", "run headless")
   .option("-v, --verbose", "more verbose logs")
@@ -15,5 +14,7 @@ program
   program
   .command("transform")
   .requiredOption("-d, --directory <directory>", "transactions directory")
+  .requiredOption("-o, --output <directory>", "directory to output transformed transaction")
+  .option("-i, --installments", "whether to process installments")
   .action((options: any) => transform(options));
 program.parse(process.argv);
