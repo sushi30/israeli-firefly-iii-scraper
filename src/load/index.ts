8b98a3a3a348180257a8ce2a2a6dd2f4042efc03
imri <imri.paran@gmail.com>
2021-02-26 18:00:28 +0200

    added logging

--- src/load/index.ts
+++ src/load/index.ts
@@ -16,18 +16,22 @@ async function awaitBatches(
 }
 
 export default async function main({ directory, host, dryRun }) {
+  console.log("reading files");
   const fromFiles = fs
     .readdirSync(directory)
     .map((f) =>
       JSON.parse(fs.readFileSync(path.join(directory, f)).toString())
     );
+  console.log("checking which transactions exist...");
   const txns = await awaitBatches(fromFiles, async (tx) => ({
     tx,
     exists: await txExists(host, tx),
   })).then((res) => res.filter(({ exists }) => !exists));
   if (dryRun) {
+    console.log("Dry run. Sending transactions to output.");
     txns.forEach(({ tx }) => console.log(JSON.stringify(tx)));
   } else {
+    console.log("sending to FireFly III...");
     await awaitBatches(txns, ({ tx }) =>
       postWrapper(`${host}/api/v1/transactions`, {
         error_if_duplicate_hash: true,
