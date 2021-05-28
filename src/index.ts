c733d4a5673db97aa8917f2385395bae12a525dd
imri <imri.paran@gmail.com>
2021-05-28 18:33:37 +0300

    added source/destination arguments

--- src/index.ts
+++ src/index.ts
@@ -8,30 +8,31 @@ import load from "./load";
 async function main() {
   program
     .version("0.1.0")
-    .command("scrape")
+    .command("scrape <destination>")
     .requiredOption("-s, --start <date>", "start date")
     .requiredOption("-t, --type <type>", "type of scraper")
     .option("-h, --headless", "run headless")
     .option("-v, --verbose", "more verbose logs")
-    .option("-d, --destination <directory>", "otuput directory")
-    .action((options: any) => extract(options));
+    .action((destination: string, options: any) =>
+      extract({ destination, ...options })
+    );
 
   program
-    .command("transform")
-    .requiredOption("-d, --directory <directory>", "transactions directory")
-    .requiredOption(
-      "-o, --output <directory>",
-      "directory to output transformed transaction"
-    )
+    .command("transform <directory> [output]")
     .option("-i, --installments", "whether to process installments")
-    .action((options: any) => transform(options));
+    .action((source: string, destination: string, options: any) =>
+      transform({ directory: source, output: destination, ...options })
+    );
 
   program
-    .command("load")
-    .requiredOption("-d, --directory <directory>", "transactions directory")
-    .requiredOption("-h, --host <host>", "FireFly III host")
+    .command("load <source>")
+    .requiredOption(
+      "-h, --host <host>",
+      "FireFly III host",
+      "http://localhost:8080"
+    )
     .option("--dry-run", "print transactions and dont send to FireFly III")
-    .action((options: any) => load(options));
+    .action((source, options: any) => load({ directory: source, ...options }));
   return await program.parseAsync(process.argv);
 }
 
